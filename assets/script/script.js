a93e451516237edee2a163bf370015b9f7df89e8
mauricechouam <lansichouamou@gmail.com>
2020-05-06 23:21:29 -0700

    first version

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+var cityname = $("#cityname");
+var tempcity = $("#tempcity");
+var humcity = $("#humcity");
+var windspeed = $("#windsped");
+var uvindex = $("#uvindex");
+var savecity = $("#savecity");
+
+
