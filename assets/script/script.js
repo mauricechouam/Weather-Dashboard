f7d365e056908d695eace1fa68a4807c99f7b06a
mauricechouam <lansichouamou@gmail.com>
2020-05-06 23:44:44 -0700

    start URL function

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,3 +6,23 @@ var uvindex = $("#uvindex");
 var savecity = $("#savecity");
 
 
+//funtion to display the Daily Weather
+
+function displaydailyweather() {
+
+    var city = $("#entercity").value;
+
+    var queryURL = "https://www.omdbapi.com/?t=" + city + "&apikey=trilogy";
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+
+
+    })
+}
+
